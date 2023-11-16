0064af056ff099efcb193104a0e4a7b1d751e0f3
yanstudy <evr.studying@gmail.com>
2023-11-16 19:52:16 +0300

    cors

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const appRouter = require('./routes/index');
 const { PORT, DB_URL } = require('./config');
 const { limiter } = require('./middlewares/limiter');
+const cors = require('./middlewares/cors');
 
 // База данных
 mongoose
@@ -18,6 +19,7 @@ mongoose
   .then(() => console.log('Connected to db'));
 
 const app = express();
+app.use(cors);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
--- config.js
+++ config.js
@@ -5,10 +5,20 @@ const {
   JWT_SECRET = 'JWT', NODE_ENV = 'production', PORT = '3000', DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb',
 } = process.env;
 
+// Список доступных урлов
+const allowedCors = [
+  'https://yanstudymovies.nomoredomainsrocks.ru',
+  'http://yanstudymovies.nomoredomainsrocks.ru',
+  'https://praktikum.tk',
+  'http://praktikum.tk',
+  'http://localhost:3000',
+];
+
 module.exports = {
   SAULT_ROUNDS,
   JWT_SECRET,
   NODE_ENV,
   PORT,
   DB_URL,
+  allowedCors,
 };
