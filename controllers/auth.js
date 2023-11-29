b77914642dd86389831144f643bc84e51088bfde
yanstudy <evr.studying@gmail.com>
2023-11-29 14:57:37 +0300

    fix: domain

--- controllers/auth.js
+++ controllers/auth.js
@@ -20,6 +20,9 @@ const createUser = (req, res, next) => {
         res.status(201).cookie('jwt', token, {
           maxAge: 3600000 * 24 * 7,
           httpOnly: true,
+          sameSite: 'None',
+          secure: true,
+          domain: 'yanstudymovies.nomoredomainsmonster.ru',
         }).send(userWithoutPassword);
       })
       .catch(next);
@@ -43,6 +46,9 @@ const login = (req, res, next) => {
           return res.status(200).cookie('jwt', token, {
             maxAge: 3600000 * 24 * 7,
             httpOnly: true,
+            sameSite: 'None',
+            secure: true,
+            domain: 'yanstudymovies.nomoredomainsmonster.ru',
           }).send({ message: 'Вы вошли в аккаунт' });
         });
     })
