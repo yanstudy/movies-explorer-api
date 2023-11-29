70b848d0c988cadd370132ec9b4ec860aa808cdf
yanstudy <evr.studying@gmail.com>
2023-11-29 10:22:19 +0300

    fix: samesite noo4

--- controllers/auth.js
+++ controllers/auth.js
@@ -46,7 +46,7 @@ const login = (req, res, next) => {
             maxAge: 3600000 * 24 * 7,
             httpOnly: true,
             sameSite: 'none',
-            secure: 'false',
+            secure: false,
           }).send({ message: 'Вы вошли в аккаунт' });
         });
     })
